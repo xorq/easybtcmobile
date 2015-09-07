9ac8262bee03bf41c403a64577b6fc3225fdcfac
xorq <xorq@hotmail.com>
2015-09-08 00:48:22 +0700

    first steps

--- js/index.js
+++ js/index.js
@@ -1,51 +1,72 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+
+var VaultView = Backbone.View.extend({
+	el: $('#vault'),
+	template: _.template($('#vault-template').text()),
+
+	render: function() {
+		this.$el.html(this.template());
+		return this;
+	}
+})
+
+var Vault = Backbone.Model.extend({
+
+})
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+		app.receivedEvent('deviceready');
+	},
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+		console.log('Received Event: ' + id);
+	},
+
+	index: function(){
+		
+	},
+
+	vault: function(){
+
+		vaultHolder = $('#vault');
+		vaultHolder.empty()	
+		
+		var vaultView = (new VaultView());
+		
+		vaultView.render().$el.appendTo(vaultHolder);	
+		vaultHolder.enhanceWithin();
+		// Initialize the sliderbutton
+		
+
+	},
 };
 
-app.initialize();
+var router = new $.mobile.Router([
+		//{ "#bus[?]\\d+": {handler: "bus", events:"bs"} },
+		//{ "#dest[?]\\S+": {handler: "dest", events:"bs"} },
+		{ "": { handler: "index", events: "bs"} },
+		{ "#vault": { handler: "vault", events: "bs"} }
+], app);
